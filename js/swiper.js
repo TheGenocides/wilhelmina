fda332f5638acd9bfeb10b66c0ac51853ed63693
Luckwut <roihan2007@gmail.com>
2024-08-10 21:14:26 +0700

    update

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,25 @@
+// Add new image inside array
 const imagePath = [
-  '.assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp'
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
+  './assets/jadwal-img/BAGAN-FUTSAL-SMP-WILHELMINA-A-VOL6A.webp',
 ];
 
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
+for (let i = 0; i < imagePath.length; i++) {
+  const imageCard = (
+    `<div class="content swiper-slide">
+      <img src="${imagePath[i]}" loading="lazy" alt="Gambar Jadwal">
+      <div class="swiper-lazy-preloader"></div>
+    </div>`
+  );
+
+  swiperWrapper.innerHTML = swiperWrapper.innerHTML + imageCard;
+}
 
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
